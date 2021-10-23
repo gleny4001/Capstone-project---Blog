31ac576a095c8dd8a891873b545f064584710a2c
Glen Yang <gleny4001@gmail.com>
2021-10-23 13:12:44 +0000

    Change secret keys

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['Secret_key']
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
 
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
